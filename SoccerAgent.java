bc0289f87fdfac8be6cb52e2243293a8db88785e
farhanmCh <farhanmchowdhury@gmail.com>
2024-11-23 20:56:52 -0500

    Changes to replicate Krislet's robocup using jason AgentSpeak BDI framework

--- SoccerAgent.java
+++ SoccerAgent.java
@@ -15,25 +15,33 @@ public class SoccerAgent extends AgArch {
     private Memory m_memory;
     private boolean m_running;
     private String m_currentIntent;
+    private boolean m_isPositioning;
+    private Agent m_agent;
+    private char m_side;
 
     public SoccerAgent(char side,
-                       int number,
-                       String playMode,
-                       Memory memory) {
-        // set up the Jason agent
-        try {
-            Agent ag = new Agent();
-            new TransitionSystem(ag, null, null, this);
-            ag.initAg();
-            ag.load(playMode + ".asl");
-            ag.addBel(Literal.parseLiteral("side(" + side + ")"));
-            ag.addBel(Literal.parseLiteral("number(" + number + ")"));
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Init error", e);
-        }
+                      int number,
+                      String playMode,
+                      Memory memory) {
         m_running = false;
         m_currentIntent = "";
         m_memory = memory;
+        m_side = side;
+        
+        // Initialize Jason agent immediately for play_on mode
+        initializeJasonAgent(side);
+    }
+
+    private void initializeJasonAgent(char side) {
+        try {
+            m_agent = new Agent();
+            new TransitionSystem(m_agent, null, null, this);
+            m_agent.initAg();
+            m_agent.load("player.asl");
+            m_agent.addBel(Literal.parseLiteral("side(" + side + ")"));
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Init error", e);
+        }
     }
 
     public String getReasoningResult() {
@@ -69,14 +77,19 @@ public class SoccerAgent extends AgArch {
     public List<Literal> perceive() {
         List<Literal> l = new ArrayList<>();
         final float TARGET_BALL_DIST = 0.9f;
-//        l.add(Literal.parseLiteral("x(10)"));
-        //TODO
+        
         ObjectInfo ball = m_memory.getObject("ball");
         ObjectInfo goalL = m_memory.getObject("goal l");
         ObjectInfo goalR = m_memory.getObject("goal r");
+
+        System.out.println("\nPerception Cycle:");
+        System.out.println("Side: " + m_side);
+        
         if (ball == null) {
+            System.out.println("Ball not visible");
             l.add(Literal.parseLiteral(Belief.BALL_UNKNOWN));
         } else {
+            System.out.println("Ball: distance=" + ball.getDistance() + ", direction=" + ball.getDirection());
             if (ball.getDistance() > TARGET_BALL_DIST) {
                 l.add(Literal.parseLiteral(Belief.BALL_FAR));
             } else {
@@ -90,18 +103,33 @@ public class SoccerAgent extends AgArch {
             }
         }
 
-        if (goalL == null) {
-            l.add(Literal.parseLiteral(Belief.GOAL_L_UNKNOWN));
-        } else {
+        // Log goal perceptions
+        String targetGoal = (m_side == 'l') ? "goal r" : "goal l";
+        System.out.println("Looking for goal: " + targetGoal);
+        
+        if (goalL != null) {
+            System.out.println("Left goal visible: distance=" + goalL.getDistance() + ", direction=" + goalL.getDirection());
             l.add(Literal.parseLiteral(Belief.GOAL_L_KNOWN));
+        } else {
+            System.out.println("Left goal not visible");
+            l.add(Literal.parseLiteral(Belief.GOAL_L_UNKNOWN));
         }
 
-        if (goalR == null) {
-            l.add(Literal.parseLiteral(Belief.GOAL_R_UNKNOWN));
-        } else {
+        if (goalR != null) {
+            System.out.println("Right goal visible: distance=" + goalR.getDistance() + ", direction=" + goalR.getDirection());
             l.add(Literal.parseLiteral(Belief.GOAL_R_KNOWN));
+        } else {
+            System.out.println("Right goal not visible");
+            l.add(Literal.parseLiteral(Belief.GOAL_R_UNKNOWN));
         }
+
+        System.out.println("Generated beliefs: " + l);
         return l;
+    }
+
+    public void startGameplay(String playMode) {
+        m_isPositioning = false;
+        initializeJasonAgent(m_side);
     }
 
     @Override
